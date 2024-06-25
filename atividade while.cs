72c116f86a663deb8a6742d68e87a993a447765c
Felipe De Souza <146121300+FelipeSoouza@users.noreply.github.com>
2024-06-25 14:17:35 -0300

    Add files via upload

---
+++ atividade while.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+              
+            Console.Write("Digite sua senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha invalida!");
+
+                Console.Write("Digite novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Senha correta!");
+
+        }
+    }
+}
