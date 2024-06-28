db176c2935a53e8e3680e82d2beecc1a2dd5032b
Felipe De Souza <146121300+FelipeSoouza@users.noreply.github.com>
2024-06-28 11:06:51 -0300

    Calc de medidas(OB1).cs

--- Classe calc medidas.cs
+++ Classe calc medidas.cs
@@ -1,15 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace OB_Csharp
 {
-    internal class Triângulo
+    internal class Program
     {
-        public double A;
-        public double B;
-        public double C;
+        static void Main(string[] args)
+        {
+            Triângulo x, y;
+
+            x = new Triângulo();
+            y = new Triângulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo x:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
     }
 }
